fe73cc49f9815c67ceff8b65a0b2284812d672db
gaiaAV32 <gaia.digregorio@avenue32.com>
2016-03-19 20:19:23 +0000

    projectpage

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ $(window).scroll(function() {
         $('a.back-to-top-button').fadeOut('fast');
     }
 });
+
+$(function() {  
+    var window_height = $(window).height(),
+       content_height = window_height - 200;
+
+    $('.scrollable-wrapper-div').height(content_height);
+});
+
+$( window ).resize(function() {
+    var window_height = $(window).height(),
+       content_height = window_height - 200;
+    $('.scrollable-wrapper-div').height(content_height);
+});
