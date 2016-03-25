4e55529b2babcce1d0ce91becc2f122466359136
gaiaAV32 <gaia.digregorio@avenue32.com>
2016-03-25 23:06:33 +0000

    good

--- js/fadein.js
+++ js/fadein.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     
         /* Check the location of each desired element */
         $('.hideme').each( function(i){
-            
+                console.log("GO TO URL");
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
@@ -21,3 +21,4 @@ $(document).ready(function() {
     });
     
 });
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+
+
+$("#linkedinIcon").click(function(){
+    console.log("GO TO URL");
+    window.open('https://uk.linkedin.com/pub/gaia-di-gregorio/44/660/bb9', '_blank');
+});
+
+$('#emailIcon').on('click', function (event) {
+    console.log("write email");
+    var email = 'gaia.digregorio@gmail.com';
+    var subject = 'Ciao Gaia!';
+    window.location = 'mailto:' + email + '?subject=' + subject;
+});
+
+
 var amountScrolled = 200;
 
 $(window).scroll(function() {
